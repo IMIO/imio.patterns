dcf937cd2cff74ec1a185df42b58980c3175fa7a
AuroreMariscal <aurore@affinitic.be>
2020-01-27 13:09:37 +0100

    Add slider

--- src/imio/patterns/browser/static/pattern/pattern.slick.js
+++ src/imio/patterns/browser/static/pattern/pattern.slick.js
@@ -1,40 +1,73 @@
 /* global require
 * Example: Slick element
-<p class="pat-imio-slick">
-</p>
+<div class="pat-imio-slick"
+  data-pat-imio-slick='{"slidesToShow": 1, "slidesToScroll": 1, "infinite": true, "dots": false, "speed": 100}'>
+    <div>slide 1</div>
+    <div>slide 2</div>
+    <div>slide 3</div>
+</div>
 */
-
-$.fn.toggleText = function (t1, t2) {
-  if (this.text() == t1) this.text(t2);
-  else this.text(t1);
-  return this;
-};
 
 define([
   'jquery',
   'underscore',
   'pat-base',
   'pat-registry',
-  'text!imio-patterns-bundle-url/templates/slick.xml',
   'imio-patterns-i18n',
   'mockup-utils',
-], function ($, _, Base, registry, SlickTemplate, _t, utils) {
+  'slick',
+], function ($, _, Base, registry, _t, utils, Slick) {
   'use strict';
 
   var SlickPattern = Base.extend({
     name: 'imio-slick',
     trigger: '.pat-imio-slick',
     parser: 'mockup',
-    template: _.template(SlickTemplate),
     defaults: {
-      textMore: _t('Read more'),
-      textLess: _t('Read less'),
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      speed: 300,
+      dots: true,
     },
     init: function () {
       var self = this;
       var template = self.$el.after(self.template({
         _t: _t,
       }));
+      self.$el.slick({
+         infinite: self.options.infinite,
+         slidesToShow: self.options.slidesToShow,
+         slidesToScroll: self.options.slidesToScroll,
+         speed: self.options.speed,
+         dots: self.options.dots,
+         responsive: [
+           {
+             breakpoint: 1024,
+             settings: {
+               slidesToShow: 3,
+               slidesToScroll: 3,
+               infinite: true,
+               dots: true
+             }
+           },
+           {
+             breakpoint: 600,
+             settings: {
+               slidesToShow: 2,
+               slidesToScroll: 2
+             }
+           },
+           {
+             breakpoint: 480,
+             settings: {
+               slidesToShow: 1,
+               slidesToScroll: 1
+             }
+           }
+         ]
+      }
+      );
     },
   });
 
